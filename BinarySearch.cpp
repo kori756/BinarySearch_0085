b9f225976db251d87099b31904fa3a3475aa209e
kori756 <khorirachmatr@gmail.com>
2026-04-16 17:44:22 +0700

    menambahkan percabangan if else

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -6,5 +6,18 @@ int nPanjang;
 int x;
 
 void input()
+{
+    while (true)
+    {
+        cout << "Masukan banyaknya elemen pada array (maksimal 10): ";
+        cin >> nPanjang;
 
-
+        if (nPanjang <= 10)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\n[!] Jumlah elemen tidak boleh lebih dari 10. Silahkan coba lagi.|n";
+        }
+    
