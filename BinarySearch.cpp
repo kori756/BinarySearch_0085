c3c868fc37a300e536be0bb6d97a8c4c672e6ec8
kori756 <khorirachmatr@gmail.com>
2026-04-16 17:51:04 +0700

    menambahkan display

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -52,3 +52,20 @@ void bubbleSortArray()
     
 }
 
+void display()
+{
+    cout << "\n===========================================\n";
+    cout << "       Elemen Array setelah di urutkan (Asc)  \n";
+    cout << "=============================================\n";
+
+    for (int j = 0; j < nPanjang; j++)
+    {
+        cout << element[j];
+        if (j < nPanjang - 1)
+        {
+            cout << " -> ";
+        }
+    }
+    cout << endl;
+}
+
